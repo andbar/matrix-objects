883d13cb580c48d157ae861327d879445c90bb7c
andbar <barringera@gmail.com>
2015-09-23 15:38:10 -0400

    Matrix class mostly working

--- matrix_math.py
+++ matrix_math.py
@@ -35,6 +35,8 @@ class Vector():
     def __eq__(self, other):
         if self.vec == other.vec:
             return True
+        else:
+            return False
 
     def dot(self, other):
         self.shape_checker(other)
@@ -49,3 +51,53 @@ class Vector():
             values.append(value ** 2)
         magn = sum(values)
         return magn ** 0.5
+
+
+class Matrix:
+
+    def __init__(self, matrix):
+        self.matrix = matrix
+        self.shape = (len(matrix), len(matrix[0]))
+
+    def matrix_row(self, row):
+        return self.matrix[row]
+
+    def matrix_col(self, col):
+        col_list = []
+        for row in self.matrix:
+            col_list.append(row[col])
+        return col_list
+
+    def matrix_scalar_multiply(self, other):
+        output_matrix = []
+        for row in self.matrix:
+            output_row = []
+            for col in row:
+                output_row.append(col * other)
+            output_matrix.append(output_row)
+        return output_matrix
+
+    def matrix_vector_multiply(self, other):
+        if len(self.matrix[0]) != other.shape:
+            raise ShapeException()
+        output_vector = []
+        for row in self.matrix:
+            output_row = []
+            for index, value in enumerate(row):
+                output_row.append(value * other.vec[index])
+            output_vector.append(sum(output_row))
+        return output_vector
+
+    def __mul__(self, other):
+        if isinstance(other, Vector):
+            return self.matrix_vector_multiply(other)
+        elif isinstance(other, self.__class__):
+            print("That's too hard for me!")
+        else:
+            return self.matrix_scalar_multiply(other)
+
+    def __eq__(self, other):
+        if self.matrix == other.matrix:
+            return True
+        else:
+            return False
